47a141b2718479fb84e9554fdcc47b25a95daea7
ArBuZzZ44 <zahar1702@mail.ru>
2023-12-29 13:59:52 +0700

    Add current user method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,9 @@ module SessionsHelper
 	def log_in(user)
 		# временные куки шифруются, а постоянные, созданные методом cookies не шифруютс сами по себе
 		session[:user_id] = user.id
+	end
+
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
 end
