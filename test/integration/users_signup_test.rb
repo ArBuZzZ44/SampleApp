56a6d4907951c9d8f52ac4ced12d7a26cce52427
ArBuZzZ44 <zahar1702@mail.ru>
2024-01-03 10:23:37 +0700

    add log in user after signup and test it

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -26,5 +26,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/show'
 		assert_equal "Welcome to the Sample App!", flash[:success]
+		assert is_logged_in?
 	end
 end
