6586bf55af440eeafc8a464e51317e621154a877
ArBuZzZ44 <zahar1702@mail.ru>
2024-01-25 11:45:41 +0700

    refactiring

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,17 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
 
+	 # Активирует аккаунт.
+	 def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Отправляет электронное письмо для активации.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
 	private
 
 	def downcase_email
